8a1998e0085995ee322d6a7da44ea99ad7d0e5b4
p4vl3n <pavlengeorgiev@gmail.com>
2022-03-14 14:48:09 +0200

    Refactored code

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,94 +3,83 @@ from time import sleep
 
 
 class TicTacToe:
+    EMPTY_FIELD = ' '
+    PLAYER_X = 'X'
+    PLAYER_O = 'O'
+
     def __init__(self, size):
         self.size = size
-        self.board = [[' ' for c in range(size)] for r in range(size)]
+        self.board = [[TicTacToe.EMPTY_FIELD for c in range(size)] for r in range(size)]
 
     def display_board(self):
         system('cls')
-        header = [str(i) if i > 0 else ' ' for i in range(self.size + 1)]
+        header = [str(i) if i > 0 else TicTacToe.EMPTY_FIELD for i in range(self.size + 1)]
         print(" | ".join(header))
         for i, r in enumerate(self.board):
             print(f'{i + 1} |', " | ".join(r))
             print('.....' * self.size)
 
     @staticmethod
-    def swap_player(player):
-        if player == 'X':
-            return 'O'
-        return 'X'
+    def swap_player(plyr):
+        if plyr == TicTacToe.PLAYER_X:
+            return TicTacToe.PLAYER_O
+        return TicTacToe.PLAYER_X
 
     # Applying player's move.
     def make_a_move(self, plyr, r, c):
         self.board[r][c] = plyr
 
     # Checking if player's selection is in valid format.
-    def valid_user_move(self, selection):
-        try:
-            row, column = list(map(int, selection))
-        except ValueError:
-            print('Invalid format. Please follow the specified format.')
-            return False
-        if row > self.size or column > self.size:  # Checking if selected row by
-            print(f'Selected row or column out of range. Please select a row and column from 1 to {self.size}.')
-            return False
-        return True
+    def valid_user_move(self):
+        while True:
+            user_selection = input('Please select a row and column number (i.e. 11, 21, 32, etc): ')
+            try:
+                r, c = list(map(int, user_selection))
+                r -= 1
+                c -= 1
+            except ValueError:
+                print('Invalid format. Please follow the specified format.')
+                continue
+            if r > self.size or c > self.size:  # Checking if selected row by
+                print(f'Selected row or column out of range. Please select a row and column from 1 to {self.size}.')
+                continue
+            if not self.board[r][c] == TicTacToe.EMPTY_FIELD:
+                print('Please select unoccupied field!')
+                continue
+            return r, c
 
-    # Checking if selected field is available.
-    def field_is_empty(self, r, c):
-        if self.board[r][c] == ' ':
-            return True
-        print('Please select unoccupied field!')
-        return False
-
-    def player_wins(self, player):
-        if self.winner_rows(player):
-            return True
-        elif self.winner_columns(player):
-            return True
-        elif self.winner_diagonals(player):
-            return True
-        return False
-
-    # Scanning all rows if we have a winner.
-    def winner_rows(self, player):
-        for row in range(self.size):
-            winner = True
-            for col in range(self.size):
-                if not self.board[row][col] == player:
-                    winner = False
-                    break
-            if winner:
+    def check_for_winner(self):
+        for sequence in self.get_sequences():
+            winner = self.winning_sequence(sequence)
+            if not winner == TicTacToe.EMPTY_FIELD:
                 return winner
-
-    # Scanning all columns if we have a winner.
-    def winner_columns(self, player):
-        for col in range(self.size):
-            winner = True
-            for row in range(self.size):
-                if not self.board[row][col] == player:
-                    winner = False
-                    break
-            if winner:
-                return winner
-
-    # Scanning the two diagonals if we have a winner.
-    def winner_diagonals(self, player):
-        winner = True
-        for i in range(self.size):
-            if not self.board[i][i] == player:
-                winner = False
-                break
-        if winner:
-            return winner
-
-        winner = True
-        for i in range(self.size):
-            if not self.board[i][-abs(i + 1)] == player:
-                winner = False
-                break
         return winner
+
+    @staticmethod
+    def winning_sequence(seq):
+        return seq[0] if len(set(seq)) == 1 else TicTacToe.EMPTY_FIELD
+
+    def get_rows(self):
+        return [row for row in self.board]
+
+    def get_columns(self):
+        columns = []
+        for c in range(self.size):
+            col = [self.board[r][c] for r in range(self.size)]
+            columns.append(col)
+        return columns
+
+    def get_diagonals(self):
+        diagonal_one = [self.board[i][i] for i in range(self.size)]
+        diagonal_two = [self.board[i][-abs(i + 1)] for i in range(self.size)]
+        return diagonal_one, diagonal_two
+
+    def get_sequences(self):
+        sequences = []
+        sequences.extend(self.get_rows())
+        sequences.extend(self.get_columns())
+        sequences.extend(self.get_diagonals())
+        return sequences
 
     # Scanning if there are empty cells/fields or if the board is full.
     def board_is_full(self):
@@ -104,20 +93,13 @@ class TicTacToe:
     # The play method initiates the game.
     def play(self):
         self.display_board()
-        player = 'X'
+        player = TicTacToe.PLAYER_X
 
         while not self.board_is_full():
-            user_selection = input(f'Player {player} choose a row number and a column number to place your move: ')
-            if not self.valid_user_move(user_selection):
-                continue
-            row, column = list(map(int, user_selection))
-            row -= 1
-            column -= 1
-            if not self.field_is_empty(row, column):
-                continue
+            row, column = self.valid_user_move()
             self.make_a_move(player, row, column)
             self.display_board()
-            if self.player_wins(player):
+            if not self.check_for_winner() == TicTacToe.EMPTY_FIELD:
                 print(f'Congratulations! Player {player} wins!')
                 break
             player = self.swap_player(player)
